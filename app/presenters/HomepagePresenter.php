7331494b0ae8ff6c099be9c582aec1147cede12c
Jaroslav Malý <jaroslav.maly@zasilkovna.cz>
2019-02-14 12:45:22 +0100

    WIP

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -7,4 +7,11 @@ use Nette;
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+	/** @var \App\Model\Orm\UsersRepository @inject */
+	public $usersRepository;
+
+	public function renderDefault() {
+		$users = $this->usersRepository->findAll();
+		var_dump($users);
+	}
 }
